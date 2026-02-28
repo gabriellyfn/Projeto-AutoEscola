ef12b457d83cdabbd8eac6d2d1ac783950c962fa
Gabrielly <gaby.gdmc@gmail.com>
2026-02-28 14:16:16 -0300

    Serviço TokenService completo

--- src/main/java/br/com/senai/s042/autoescolas042/controller/UsuarioController.java
+++ src/main/java/br/com/senai/s042/autoescolas042/controller/UsuarioController.java
@@ -1,4 +1,23 @@
 package br.com.senai.s042.autoescolas042.controller;
 
+import br.com.senai.s042.autoescolas042.domain.usuario.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+   // @PostMapping
+    // @Transactional
+   // public String cadastrarUsuario(@RequestBody DadosCadastroUsuario dadosUsuario) {
+
+    //}
 }
